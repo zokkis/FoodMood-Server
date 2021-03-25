28ee399fc848a5689e87f5191bf945552c4435b7
gitZokki <timniclasoel@gmail.com>
2021-03-25 20:44:34 +0100

    logging to file

--- logger.js
+++ logger.js
@@ -1,18 +1,33 @@
+const fs = require('fs');
+
+if (!fs.existsSync('./logs')) {
+	fs.mkdirSync('./logs');
+}
+const logStream = fs.createWriteStream("./logs/logs.log", { flags: 'a+' });
+const warnStream = fs.createWriteStream("./logs/warns.log", { flags: 'a+' });
+const errorStream = fs.createWriteStream("./logs/erros.log", { flags: 'a+' });
+
 module.exports = class Logger {
 	constructor(prefix) {
 		this.prefix = prefix;
 	}
 
 	log(...message) {
-		console.log(this.#getLoggingPrefix('\x1b[37m', 'LOG'), ...message, '\x1b[0m');
+		const prefix = this.#getLoggingPrefix('\x1b[37m', 'LOG');
+		console.log(prefix, ...message, '\x1b[0m');
+		this.#appendTo(logStream, prefix, message);
 	};
 
 	warn(...warn) {
-		console.warn(this.#getLoggingPrefix('\x1b[33m', 'WARN'), ...warn, '\x1b[0m');
+		const prefix = this.#getLoggingPrefix('\x1b[33m', 'WARN');
+		console.warn(prefix, ...warn, '\x1b[0m');
+		this.#appendTo(warnStream, prefix, warn);
 	};
 
 	error(...error) {
-		console.error(this.#getLoggingPrefix('\x1b[31m', 'ERROR'), ...error, '\x1b[0m');
+		const prefix = this.#getLoggingPrefix('\x1b[31m', 'ERROR');
+		console.error(prefix, ...error, '\x1b[0m');
+		this.#appendTo(errorStream, prefix, error);
 	};
 
 	#getLoggingPrefix(color, loggingType) {
@@ -24,5 +39,13 @@ module.exports = class Logger {
 		const seconds = String(date.getSeconds()).padStart(2, '0');
 		const milliSeconds = String(date.getMilliseconds()).padStart(3, '0');
 		return `${color}[${date.getFullYear()}/${month}/${day}, ${hours}:${minutes}:${seconds}.${milliSeconds}] [${this.prefix}] [${loggingType}]`;
+	}
+
+	#appendTo(stream, prefix, arr) {
+		stream.write('\n' + prefix);
+		arr.forEach(a => {
+			const toWrite = typeof a !== 'object' ? a : JSON.stringify(a);
+			stream.write(' ' + toWrite);
+		});
 	}
 }
