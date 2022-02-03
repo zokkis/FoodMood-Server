5605eef4d1ec921ab3904bfd20b9bdac14ae8c95
gitZokki <timniclasoel@gmail.com>
2022-02-03 22:23:20 +0100

    Port for ecosystem-file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-	apps: [{
-		name: 'server',
-		script: './dist/server/server.js',
-		env: {
-			NODE_ENV: 'development',
+	apps: [
+		{
+			name: 'server',
+			script: './dist/server/server.js',
+			env: {
+				NODE_ENV: 'development',
+				PORT: 8080,
+			},
+			// eslint-disable-next-line @typescript-eslint/naming-convention
+			env_production: {
+				NODE_ENV: 'production',
+				PORT: 8080,
+			},
 		},
-		// eslint-disable-next-line @typescript-eslint/naming-convention
-		env_production: {
-			NODE_ENV: 'production',
-		}
-	}]
+	],
 };
